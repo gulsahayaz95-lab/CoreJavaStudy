bc371cfbb795b5a70c525043d9b44aa3901fc159
Gülşah Ayaz <gulsahayaz95@gmail.com>
2026-03-02 00:33:48 +0300

    "First Project Homework"

---
+++ src/Homeworks/PraticLessonHomework.java
@@ -0,0 +1,27 @@
+package Homeworks;
+
+public class PraticLessonHomework {
+    public static void main(String[] args) {
+        /**
+         Görev: Bir kişinin yaşına göre sinema bileti fiyatını hesaplayan bir Java programı yazın.
+         Sinema bileti fiyatı 60 TL'dir. Yaşa göre aşağıdaki indirimler uygulanmaktadır:
+
+         - 18 yaşından küçükler: 12 TL indirim 60-12
+         - 18-60 yaş arası: 6 TL indirim 60-6
+         - 60 yaşından büyükler: 9 TL indirim 60-9
+
+         Kullanıcıdan yaş bilgisi alarak indirimli bilet fiyatını ekrana yazdırın.
+         */
+
+
+
+        //ÖDEVİ YAP!!!!!
+
+
+
+
+
+
+
+    }
+}
